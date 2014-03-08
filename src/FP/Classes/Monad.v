cb85ef19b020d68fe15737b1490c5499ef40b92f
David Darais <david.darais@gmail.com>
2014-03-08 16:59:58 -0500

    proved the galois connection between monadic WF function spaces (GaloisWF)... although I might actually just want standard Galois between (wf A) and (wf B)

--- src/FP/Classes/Monad.v
+++ src/FP/Classes/Monad.v
@@ -29,19 +29,56 @@ Section Monad.
   Definition extend {A B} : dom ((A ⇒ m B) ⇒ (m A ⇒ m B)) := λ f aM → bind ∙ aM ∙ f.
 
   Definition mcompose {A B C} : dom ((B ⇒ m C) ⇒ (A ⇒ m B) ⇒ (A ⇒ m C)) := λ g f a → 
-    b ← f ∙ a ;;
-    g ∙ b.
-  
+    bind ∙ (f ∙ a) ∙ g.
   Definition mmap {A B} : dom ((A ⇒ B) ⇒ (m A ⇒ m B)) := λ f aM →
     a ← aM ;;
     ret $ f ∙ a.
   
   Definition seq {A B} : dom (m A ⇒ m B ⇒ m B) := λ aM bM → _ ← aM ;; bM.
+  
+  Definition kret {A B} : dom ((A ⇒ B) ⇒ (A ⇒ m B)) := λ f → ret ⊙ f.
 End Monad.
 Notation "k =<< e" := (extend ∙ k ∙ e) (at level 70, right associativity).
 Notation "g m⊙ f" := (mcompose ∙ g ∙ f) (at level 60, right associativity).
 Notation "e₁  ;; e₂" := (_ ← e₁ ;; e₂) (at level 100, right associativity).
 
+Section Laws.
+  Context {m} `{! Monad m }.
+
+  Definition mcompose_associativity {A B C D} 
+  (h:dom (C ⇒ m D)) (g:dom (B ⇒ m C)) (f:dom (A ⇒ m B)) : ((h m⊙ g) m⊙ f) ≃ (h m⊙ (g m⊙ f)).
+  Proof.
+    repeat (Re fail || MonadRewrite ; qproper_elim).
+  Qed.
+
+  Definition mcompose_left_unit {A B} (f:dom (A ⇒ m B)) : (ret m⊙ f) ≃ f.
+  Proof.
+    repeat (Re fail || MonadRewrite ; qproper_elim).
+  Qed.
+  
+  Definition mcompose_right_unit {A B} (f:dom (A ⇒ m B)) : (f m⊙ ret) ≃ f.
+  Proof.
+    repeat (Re fail || MonadRewrite ; qproper_elim).
+  Qed.
+End Laws.
+
+Global Opaque kret.
+Global Opaque mcompose.
+
+Ltac KleisliRewrite :=
+  match goal with
+  | |- ⟨ (?h m⊙ ?g) m⊙ ?f ∈ _ |_| _ ⟩ => ReplaceBy (mcompose_associativity h g f)
+  | |- ⟨ ret m⊙ ?f ∈ _ |_| _ ⟩ => ReplaceBy (mcompose_left_unit f)
+  | |- ⟨ ?f m⊙ ret ∈ _ |_| _ ⟩ => ReplaceBy (mcompose_right_unit f)
+  end.
+
+Ltac KleisliRewrite2 :=
+  match goal with
+  | |- ⟨ ?h m⊙ ?g m⊙ ?f ∈ _ |_| _ ⟩ => ReplaceBy (symmetry (mcompose_associativity h g f))
+  | |- ⟨ ret m⊙ ?f ∈ _ |_| _ ⟩ => ReplaceBy (mcompose_left_unit f)
+  | |- ⟨ ?f m⊙ ret ∈ _ |_| _ ⟩ => ReplaceBy (mcompose_right_unit f)
+  end.
+  
 Class MonadPlus m `{! Monad m } :=
   { mzero : forall {A}, dom (m A)
   ; mplus : forall {A}, dom (m A ⇒ m A ⇒ m A)
